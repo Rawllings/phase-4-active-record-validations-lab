df77ec1a76e49bb2ecc80322868aae017636e255
Rawllings Kiprop <rutojr01@gmail.com>
2023-03-16 13:29:05 +0300

    Completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, presence: true, length: {minimum: 250}
+    validates :summary, presence: true, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+
+    validate :facts 
+
+    def facts
+        if title == "True Facts"
+            errors.add(:title, "can't be clickbait")
+        end
+    end
 end
